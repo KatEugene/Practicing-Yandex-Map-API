79c70b780998991249b068f8aa0d22259052470a
KatEugene <katasonoveugene@yandex.ru>
2020-05-06 19:04:26 +0300

    Version 3.0 Added nearest pharmacy

---
+++ dist.py
@@ -0,0 +1,17 @@
+import math
+
+
+def distance(a, b):
+    degree_to_meters_factor = 111 * 1000
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    radians_latitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_latitude)
+
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    result = math.sqrt(dx * dx + dy * dy)
+
+    return result
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import requests
 from io import BytesIO
 from PIL import Image
 from scale import scope
+from dist import distance
 
 address = " ".join(list(map(str.strip, sys.stdin)))
+
+# Получаем координаты заданного объекта
 
 geo_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
@@ -15,7 +18,6 @@ geo_params = {
 }
 
 response = requests.get(geo_api_server, params=geo_params)
-
 json_response = response.json()
 
 geo_obj = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
@@ -25,14 +27,46 @@ geo_obj_longitude, geo_obj_latitude = geo_obj_coordinates.split(" ")
 
 delta_x, delta_y = scope(geo_obj)
 
+objects1 = [geo_obj_longitude, geo_obj_latitude, "pm2rdm1"]
+
+# Получаем координаты ближайшей аптеки
+
+search_api_server = "http://search-maps.yandex.ru/v1/"
+
+search_params = {
+    "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
+    "text": "Аптеки",
+    "ll": f"{geo_obj_longitude},{geo_obj_latitude}",
+    "lang": "ru_RU"
+}
+
+response = requests.get(search_api_server, params=search_params)
+json_response = response.json()
+
+pharmacy_longitude = json_response["features"][0]["geometry"]["coordinates"][0]
+pharmacy_latitude = json_response["features"][0]["geometry"]["coordinates"][1]
+
+objects2 = [pharmacy_longitude, pharmacy_latitude, "pm2rdm1"]
+
+# Формируем итоговый запрос
+
+objects = list(map(str, objects1 + objects2))
+
 map_params = {
     "ll": ",".join([geo_obj_longitude, geo_obj_latitude]),
     "spn": ",".join([str(delta_x), str(delta_y)]),
-    "pt": ",".join([geo_obj_longitude, geo_obj_latitude, "pm2rdm1"]),
+    "pt": ",".join(objects),
     "l": "map"
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
+
+object_coordinates = (float(geo_obj_longitude), float(geo_obj_latitude))
+pharmacy_coordinates = (pharmacy_longitude, pharmacy_latitude)
+
+_distance = round(distance(object_coordinates, pharmacy_coordinates) / 1000, 2)
+
+print(f"Расстояние между вашим расположением и ближайшей аптекой составляет {_distance} километров")
 
 Image.open(BytesIO(response.content)).show()
