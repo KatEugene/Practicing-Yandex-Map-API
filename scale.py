9070ee1aa648720f8a94983ca2fc5e94f5f7ce91
KatEugene <katasonoveugene@yandex.ru>
2020-05-06 16:57:55 +0300

    Edited scale function (coefficient now is 0.5)

--- scale.py
+++ scale.py
@@ -3,7 +3,7 @@ def scope(geo_object):
     lower_corner = list(map(float, envelope["lowerCorner"].split()))
     upper_corner = list(map(float, envelope["upperCorner"].split()))
 
-    _delta_x = (upper_corner[0] - lower_corner[0])
-    _delta_y = (upper_corner[1] - lower_corner[1])
+    _delta_x = (upper_corner[0] - lower_corner[0]) * 0.5
+    _delta_y = (upper_corner[1] - lower_corner[1]) * 0.5
 
     return _delta_x, _delta_y
